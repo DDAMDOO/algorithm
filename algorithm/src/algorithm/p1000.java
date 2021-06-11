ccf04d34469a8926b87bc515d9b20faa42eb83c1
DDAMDOO <sjl9524@naver.com>
2021-06-11 09:51:56 +0900

    백준 1000번

---
+++ algorithm/src/algorithm/p1000.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class p1000 {
+	public static void main(String argv[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+}
