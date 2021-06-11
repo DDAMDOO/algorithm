f39fc5f192a34f399048eb5bf0e9493c7ff33f5b
DDAMDOO <sjl9524@naver.com>
2021-06-11 10:06:01 +0900

    백준1008번

---
+++ algorithm/src/algorithm/p1008.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class p1008 {
+	public static void main(String argv[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a / b);
+	}
+}
