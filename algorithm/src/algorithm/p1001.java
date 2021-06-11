53ca9ce3698f93c532149232a3d3127c16424b61
DDAMDOO <sjl9524@naver.com>
2021-06-11 09:58:50 +0900

    백준1001번

---
+++ algorithm/src/algorithm/p1001.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class p1001 {
+	public static void main(String argv[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a - b);
+	}
+}
