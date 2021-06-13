54dc141725e5313ec1a0445f772fafeecef1bdab
DDAMDOO <sjl9524@naver.com>
2021-06-13 11:57:46 +0900

    백준 1157번

---
+++ algorithm/src/algorithm/p1157.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class p1157 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] ary = new int[26];
+		int max = 0;
+		int idx = 0;
+		String s = br.readLine();
+
+		for (int i = 0; i < s.length(); i++) {
+			ary[((int) (s.charAt(i)) - 65) % 32]++;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (max < ary[i]) {
+				max = ary[i];
+				idx = i;
+			}
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if(max == ary[i] && i != idx) {
+				idx = -2;
+			}
+		}
+		System.out.println((char) (idx + 65));
+	}
+}
--- algorithm/src/algorithm/p1330.java
+++ algorithm/src/algorithm/p1330.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class p1330 {
 
-	public static void main(String args[]) {		
+	public static void main(String args[]) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
