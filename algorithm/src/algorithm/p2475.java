acda91ed7c5053253e173b4535b1a80472216869
DDAMDOO <sjl9524@naver.com>
2021-06-15 13:39:09 +0900

    백준 2475번

---
+++ algorithm/src/algorithm/p2475.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class p2475 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int a;
+		int sum = 0;
+
+		for (int i = 0; i < 5; i++) {
+			a = sc.nextInt();
+			sum += a * a;
+		}
+		System.out.println(sum % 10);
+	}
+}
