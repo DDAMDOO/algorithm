590c8275829756e94cde138025faed57cdc589c2
DDAMDOO <sjl9524@naver.com>
2021-06-15 16:58:01 +0900

    백준 2742번

---
+++ algorithm/src/algorithm/p2742.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class p2742 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		for (int i = a; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
