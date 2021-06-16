21d5504d46159f4c350a90d1d5384809d1bbc9c3
DDAMDOO <sjl9524@naver.com>
2021-06-17 00:18:05 +0900

    백준 15829번 100점 답안

--- algorithm/src/algorithm/class2/p15829.java
+++ algorithm/src/algorithm/class2/p15829.java
@@ -9,14 +9,16 @@ public class p15829 {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		String s = sc.next();
+		long mod = 1234567891;
 		long b = 1;
-		int sum = 0;
+		long sum = 0;
 
 		for (int i = 0; i < s.length(); i++) {
-			sum += (int) (s.charAt(i) - 96) * b;
+			long k = ((s.charAt(i) - 96) * b) % mod;
+			sum += k;
+			sum = sum % mod;
 			b *= 31;
-			b = b % 1234567891;
-			sum = sum % 1234567891;
+			b = b % mod;
 		}
 		System.out.println(sum);
 	}
