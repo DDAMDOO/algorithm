1a396cc8a3c040a4f33da2d4ffbef17b30d5dc21
DDAMDOO <sjl9524@naver.com>
2021-06-23 09:55:04 +0900

    백준 2609번

---
+++ algorithm/src/algorithm/class2/p2609.java
@@ -0,0 +1,23 @@
+package algorithm.class2;
+
+import java.util.Scanner;
+
+public class p2609 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a * b;
+		
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		System.out.println(a);
+		System.out.println(c/a);
+	}
+
+}
