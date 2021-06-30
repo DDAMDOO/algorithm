42436daaa4d6c4860868aa87f15ceb58e5d8c836
DDAMDOO <sjl9524@naver.com>
2021-06-30 14:20:18 +0900

    백준 11650번

---
+++ algorithm/src/algorithm/class2/p11650.java
@@ -0,0 +1,42 @@
+package algorithm.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class p11650 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int a = Integer.parseInt(br.readLine());
+		int[][] ary = new int[a][2];
+
+		for (int i = 0; i < a; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			ary[i][0] = Integer.parseInt(st.nextToken());
+			ary[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(ary, new Comparator<int[]>() {
+			public int compare(int[] a, int[] b) {
+				if (a[0] == b[0]) {
+					return a[1] - b[1];
+				} else {
+					return a[0] - b[0];
+				}
+			}
+		});
+
+		for (int i = 0; i < a; i++) {
+			sb.append(ary[i][0] + " " + ary[i][1] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
