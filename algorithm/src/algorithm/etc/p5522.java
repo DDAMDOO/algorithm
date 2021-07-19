a8e4257308fb00d970a072095de6390dad76c58b
DDAMDOO <sjl9524@naver.com>
2021-07-19 21:02:33 +0900

    백준 5522번

---
+++ algorithm/src/algorithm/etc/p5522.java
@@ -0,0 +1,19 @@
+package algorithm.etc;
+
+import java.util.Scanner;
+
+public class p5522 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+
+		System.out.println(a + b + c + d + e);
+	}
+}
