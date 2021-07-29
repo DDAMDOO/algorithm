137fd831012a7ea6a85ece283a5ff72d17e5e7d9
DDAMDOO <sjl9524@naver.com>
2021-07-29 15:40:53 +0900

    프로그래머스 k번째 수

--- algorithm/src/algorithm/programmers/p42748.java
+++ algorithm/src/algorithm/programmers/p42748.java
@@ -1,10 +1,20 @@
 package algorithm.programmers;
 
+import java.util.*;
 public class p42748 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	class Solution {
+	    public int[] solution(int[] array, int[][] commands) {
+	        int[] answer = new int[commands.length];
+	        
+	        for(int i=0;i<commands.length;i++){
+	            int[] ary = new int [commands[i][1]-commands[i][0]+1];
+	            for(int j=0;j<ary.length;j++){
+	                ary[j] = array[commands[i][0]-1+j];
+	            }
+	            Arrays.sort(ary);
+	            answer[i] = ary[commands[i][2]-1];
+	        }
+	        return answer;
+	    }
 	}
-
 }
