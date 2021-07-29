0811a7999462aebc2cea502f2ec5e17203c94617
DDAMDOO <sjl9524@naver.com>
2021-07-29 21:37:12 +0900

    프로그래머스 큰수찾기 수정

--- algorithm/src/algorithm/programmers/p42746.java
+++ algorithm/src/algorithm/programmers/p42746.java
@@ -2,18 +2,20 @@ package algorithm.programmers;
 
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        for(int i=0;i<commands.length;i++){
-            int[] ary = new int [commands[i][1]-commands[i][0]+1];
-            for(int j=0;j<ary.length;j++){
-                ary[j] = array[commands[i][0]-1+j];
-            }
-            Arrays.sort(ary);
-            answer[i] = ary[commands[i][2]-1];
-        }
-        return answer;
-    }
+public class p42746 {
+	class Solution {
+		public int[] solution(int[] array, int[][] commands) {
+			int[] answer = new int[commands.length];
+
+			for (int i = 0; i < commands.length; i++) {
+				int[] ary = new int[commands[i][1] - commands[i][0] + 1];
+				for (int j = 0; j < ary.length; j++) {
+					ary[j] = array[commands[i][0] - 1 + j];
+				}
+				Arrays.sort(ary);
+				answer[i] = ary[commands[i][2] - 1];
+			}
+			return answer;
+		}
+	}
 }
