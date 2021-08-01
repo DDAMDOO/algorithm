5d44df4d5750673459974a3601e573d0824b28e0
DDAMDOO <sjl9524@naver.com>
2021-08-01 19:01:42 +0900

    퍼뮤테이션

--- algorithm/src/algorithm/etc/permutation_1.java
+++ algorithm/src/algorithm/etc/permutation_1.java
@@ -1,33 +1,31 @@
 package algorithm.etc;
 
 public class permutation_1 {
-	static void print(int[] arr, int r) {
-		for (int i = 0; i < r; i++)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-	
-	static void per2(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
+	static void per2(int[] ary, int[] output, boolean[] visited, int depth, int n, int r) {
 		if (depth == r) {
-			print(output, r);
+			for (int i = 0; i < r; i++)
+				System.out.print(output[i] + " ");
+			System.out.println();
 			return;
 		}
 
 		for (int i = 0; i < n; i++) {
 			if (visited[i] != true) {
 				visited[i] = true;
-				output[depth] = arr[i];
-				per2(arr, output, visited, depth + 1, n, r);
-				output[depth] = 0; // 이 줄은 없어도 됨
+				output[depth] = ary[i];
+				per2(ary, output, visited, depth + 1, n, r);
 				visited[i] = false;
-				;
 			}
 		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] ary = {1, 2, 3};
+		int[] rst = new int[ary.length];
+		boolean[] visited = new boolean[ary.length];
+		
+		per2(ary, rst, visited, 0, 3, 2);
 	}
 
 }
