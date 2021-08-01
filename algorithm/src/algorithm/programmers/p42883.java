a7708d48a2254b9ee7773dae5e0ba549feecb62a
DDAMDOO <sjl9524@naver.com>
2021-08-01 11:14:14 +0900

    프로그래머스 큰수만들기

--- algorithm/src/algorithm/programmers/p42883.java
+++ algorithm/src/algorithm/programmers/p42883.java
@@ -1,10 +1,11 @@
 package algorithm.programmers;
 
 public class p42883 {
+
 	class Solution {
-		public String solution(String number, int k) {
-			StringBuilder answer = new StringBuilder();
-			int idx = 0;
+	    public String solution(String number, int k) {
+	        StringBuilder answer = new StringBuilder();
+	        int idx = 0;
 			int max = 0;
 			for (int i = 0; i < number.length() - k; i++) {
 				max = 0;
@@ -16,7 +17,8 @@ public class p42883 {
 				}
 				answer.append(max);
 			}
-			return answer.toString();
-		}
+	        
+	        return answer.toString();
+	    }
 	}
 }
