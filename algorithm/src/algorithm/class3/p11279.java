ad8c5606d4582dc183cb28cb0d089c0ddb4472c4
DDAMDOO <sjl9524@naver.com>
2021-08-11 00:16:31 +0900

    백준 11279번

---
+++ algorithm/src/algorithm/class3/p11279.java
@@ -0,0 +1,30 @@
+package algorithm.class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class p11279 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		int a = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < a; i++) {
+			int b = Integer.parseInt(br.readLine());
+			if (b == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.add(b);
+			}
+		}
+	}
+}
