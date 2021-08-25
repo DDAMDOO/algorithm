a55b496ab9614e644b1a87935994cb2511f9774c
DDAMDOO <sjl9524@naver.com>
2021-08-25 20:37:15 +0900

    백준 7569번 중간커밋

---
+++ algorithm/src/algorithm/class3/p7569.java
@@ -0,0 +1,43 @@
+package algorithm.class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Node7569 {
+	int x;
+	int y;
+	int z;
+	int cnt;
+
+	Node7569(int x, int y, int cnt) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.cnt = cnt;
+	}
+}
+
+public class p7569 {
+	public static boolean[][][] visited;
+	public static int[][][] ary;
+	public static int a;
+	public static int b;
+	public static int h;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		
+	}
+
+	
+}
