64c4730de637c68d7a7d5f92cc1765c885bd4819
DDAMDOO <sjl9524@naver.com>
2021-08-30 23:10:03 +0900

    7569 인덱스 수정해야함

--- algorithm/src/algorithm/class3/p7569.java
+++ algorithm/src/algorithm/class3/p7569.java
@@ -11,18 +11,15 @@ class Node7569 {
 	int x;
 	int y;
 	int z;
-	int cnt;
 
-	Node7569(int x, int y, int z, int cnt) {
+	Node7569(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		this.cnt = cnt;
 	}
 }
 
 public class p7569 {
-	public static int[][][] cnt;
 	public static int[][][] ary;
 	public static int a;
 	public static int b;
@@ -30,17 +27,20 @@ public class p7569 {
 	public static int[] nx = { 0, 0, 0, 0, 1, -1 };
 	public static int[] ny = { 0, 0, 1, -1, 0, 0 };
 	public static int[] nz = { 1, -1, 0, 0, 0, 0 };
+	public static Queue<Node7569> q;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		a = Integer.parseInt(st.nextToken());
 		b = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
 
 		ary = new int[h + 1][b + 1][a + 1];
-		cnt = new int[h + 1][b + 1][a + 1];
+
+		q = new LinkedList<Node7569>();
 
 		for (int i = 1; i <= h; i++) {
 			for (int j = 1; j <= b; j++) {
@@ -49,47 +49,54 @@ public class p7569 {
 					ary[i][j][k] = Integer.parseInt(st.nextToken());
 
 					if (ary[i][j][k] == 1) {
-						bfs(i, j, k);
+						q.add(new Node7569(i, j, k));
 					}
 				}
 			}
 		}
-	}
+		bfs();
 
-	public static void bfs(int z, int y, int x) {
-		Queue<Node7569> q = new LinkedList<>();
-
-		q.add(new Node7569(z, y, x, 0));
-
-		while (!q.isEmpty()) {
-			Node7569 n = q.poll();
-
-			int cnt = n.cnt + 1;
-
-			for (int a = 0; a < 6; a++) {
-				if (ary[z + nz[a]][y + ny[a]][x + nx[a]] == 0) {
-					ary[z + nz[a]][y + ny[a]][x + nx[a]] = 1;
-					q.add(new Node7569(z + nz[a], y + ny[a], x + nx[a], cnt));
-					ary[z + nz[a]][y + ny[a]][x + nx[a]] = cnt;
-				}
-			}
-		}
-		
 		int rst = Integer.MIN_VALUE;
-		
-		for (int i = 1; i <= z; i++) {
-			for (int j = 1; j <= y; j++) {
-				for (int k = 1; k <= x; k++) {
+
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= b; j++) {
+				for (int k = 1; k <= a; k++) {
 					if (ary[i][j][k] == 0) {
 						System.out.println(-1);
 						return;
 					}
-					if (rst < ary[i][j][k]) {
-						rst = ary[i][j][k];
+					rst = Math.max(rst, ary[i][j][k]);
+//					System.out.print(ary[i][j][k] + " ");
+				}
+//				System.out.println();
+			}
+		}
+//		System.out.println(rst);
+		return;
+	}
+
+	public static void bfs() {
+		while (!q.isEmpty()) {
+			Node7569 n = q.poll();
+
+			int x = n.x;
+			int y = n.y;
+			int z = n.z;
+			
+			for (int i = 0; i < 6; i++) {
+				int xx = x + nx[i];
+				int yy = y + ny[i];
+				int zz = z + nz[i];
+
+				if (xx > 0 && xx <= h && yy > 0 && yy <= b && zz > 0 && zz <= a) {	
+					if (ary[xx][yy][zz] == 0) {
+						System.out.println(xx + " "+ yy + " " + zz);
+						q.add(new Node7569(xx, yy, zz));
+						ary[xx][yy][zz] = ary[n.x][n.y][n.z] + 1;
 					}
 				}
 			}
+			System.out.println();
 		}
-		System.out.println(rst);
 	}
 }
