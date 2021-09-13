d43e0196d0fc1e2586ed3763b6ead1e8dab2f698
DDAMDOO <sjl9524@naver.com>
2021-09-13 22:13:50 +0900

    불필요 코드 삭제

--- algorithm/src/algorithm/class3/p11403fw.java
+++ algorithm/src/algorithm/class3/p11403fw.java
@@ -3,8 +3,6 @@ package algorithm.class3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class p11403fw {
